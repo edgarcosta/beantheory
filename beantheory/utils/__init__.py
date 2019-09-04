b23fda48a14394c6c45d7139f829ef165ab6a336
Edgar Costa <edgarc@mit.edu>
2019-09-04 15:28:31 -0400

    ups

--- beantheory/utils/__init__.py
+++ beantheory/utils/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['TableParser', 'root_path', 'requests_retry_session']
+__all__ = ['TableParser', 'root_path']
 
 from .root_path import root_path
 from .tableparser import TableParser
-from .requests_retry_session import requests_retry_session
