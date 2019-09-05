703a63c58f0600124e5bb1482d3a0d7d01e8b881
Edgar Costa <edgarc@mit.edu>
2019-09-05 00:18:11 -0400

    typo

--- beantheory/__init__.py
+++ beantheory/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __version__ = '0.0.1'
-all = ['talks, yaml_talks, yaml_meta, yaml_all']
+all = ['talks', 'yaml_talks', 'yaml_meta', 'yaml_all']
 from .app import talks, yaml_talks, yaml_meta, yaml_all
+#assert talks
+#assert yaml_talks
+#assert yaml_meta
+#assert yaml_all
 
