cec9b84dbadd071da3fb4af69dbc76eb21ff0cbb
Edgar Costa <edgarc@mit.edu>
2019-09-05 00:17:45 -0400

    paragraph parser

--- beantheory/utils/tableparser.py
+++ beantheory/utils/tableparser.py
@@ -57,3 +57,75 @@ class TableParser(HTMLParser):
         if self.table_row is not None:
             self.table.append(self.table_row)
         HTMLParser.close(self)
+
+
+class DataParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.data = []
+    def output(self):
+        return ''.join(self.data)
+
+    def handle_data(self, data):
+        self.data.append(data)
+    def handle_entityref(self, name):
+        c = unichr(name2codepoint[name])
+        if self.data is not None:
+            self.data.append(c)
+
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = unichr(int(name[1:], 16))
+        else:
+            c = unichr(int(name))
+        if self.data is not None:
+            self.data.append(c)
+
+class ParagraphParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.table = []
+        self.data = []
+    def handle_starttag(self, tag, attrs):
+        if tag == 'p':
+            if self.data:
+                self.table.append("".join(self.data))
+            self.data = []
+
+        if tag == 'br':
+            self.data.append(" ")
+
+
+
+    def handle_endtag(self, tag):
+        if tag == 'p':
+            if self.data:
+                self.table.append("".join(self.data))
+            self.data = []
+
+    def handle_data(self, data):
+        if self.data is not None:
+            self.data.append(data)
+
+    def handle_entityref(self, name):
+        c = unichr(name2codepoint[name])
+        if self.data is not None:
+            self.data.append(c)
+
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = unichr(int(name[1:], 16))
+        else:
+            c = unichr(int(name))
+        if self.data is not None:
+            self.data.append(c)
+
+def rawtext_to_paragraphlist(text):
+    # first unescape html
+    parser = DataParser()
+    parser.feed(text)
+    # convert paragraphs to lines
+    pparser = ParagraphParser()
+    pparser.feed(parser.output())
+    return pparser.table
+
